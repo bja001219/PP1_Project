59323683b1767527abae885301daa91eec87bbe7
bja00 <bja001219@hotmail.com>
2023-09-12 18:32:16 +0900

    간단한 설명 추가

--- src/main/java/com/mycom/word/Main.java
+++ src/main/java/com/mycom/word/Main.java
@@ -3,6 +3,6 @@ package com.mycom.word;
 public class Main {
 
     public static void main(String[] args) {
-        new WordManager().start();
+        new WordManager().start(); //WordManager를 시작하여 프로그램 실행
     }
 }
--- src/main/java/com/mycom/word/Word.java
+++ src/main/java/com/mycom/word/Word.java
@@ -7,7 +7,7 @@ public class Word {
     private String meaning;
 
     Word(){}
-    Word(int Id, int level, String word, String meaning){
+    Word(int Id, int level, String word, String meaning){ //한 단어의 모든 정보를 Word에 저장.
         this.Id = Id;
         this.level = level;
         this.word = word;
@@ -40,7 +40,7 @@ public class Word {
     }
 
     @Override
-    public String toString() {
+    public String toString() { //level의 숫자와 같은 갯수의 *로 레벨 표시
         String slevel = "";
         for(int i = 0; i <level ; i++) {
             slevel += "*";
